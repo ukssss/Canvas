891c542a975e82523d35e407ab47d38e163adeef
ukss <86929961+ukssss@users.noreply.github.com>
2022-10-30 22:43:12 +0900

    #2 레이아웃 변경

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const paintWidth = document.getElementById("paintWidth");
 const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const stroke = document.getElementById("stroke");
 const fill = document.getElementById("fill");
 const erase = document.getElementById("erase");
 const refresh = document.getElementById("refresh");
 const fileInput = document.getElementById("file");
+const text = document.getElementById("text");
+const save = document.getElementById("save");
 
-const CANVAS_DEFAULT = 800;
+const CANVAS_DEFAULT = 500;
 const ERASE_COLOR = "#ffffff";
 
 canvas.width = CANVAS_DEFAULT;
 canvas.height = CANVAS_DEFAULT;
 ctx.lineWidth = 5;
+ctx.lineCap = "round";
 
 let setPaint = false;
 let setMode = false;
@@ -39,9 +44,20 @@ function onStopPaint() {
   setPaint = false;
 }
 
+function onPaintWidth(event) {
+  ctx.lineWidth = event.target.value;
+}
+
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorClick = event.srcElement.dataset.color;
+  ctx.strokeStyle = colorClick;
+  ctx.fillStyle = colorClick;
+  color.value = colorClick;
 }
 
 function onStrokeMode() {
@@ -75,14 +91,37 @@ function onFileUpload(event) {
   };
 }
 
+function onTextInput(event) {
+  const textInput = text.value;
+  if (textInput !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "68px 'Press Start 2p'";
+    ctx.fillText(textInput, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveFile() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "paint.png";
+  a.click();
+}
+
 canvas.addEventListener("mousemove", onMovePaint);
 canvas.addEventListener("mousedown", onStartPaint);
 canvas.addEventListener("mouseup", onStopPaint);
 canvas.addEventListener("mouseleave", onStopPaint);
+canvas.addEventListener("dblclick", onTextInput);
 
+paintWidth.addEventListener("change", onPaintWidth);
 color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 stroke.addEventListener("click", onStrokeMode);
 fill.addEventListener("click", onFillMode);
 erase.addEventListener("click", onEraseMode);
 refresh.addEventListener("click", onRefreshMode);
 file.addEventListener("change", onFileUpload);
+save.addEventListener("click", onSaveFile);
